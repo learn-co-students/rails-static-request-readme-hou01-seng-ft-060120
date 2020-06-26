718d8a605ea59aec801af2499f239c75752da3e9
reynaldoayala062 <reynaldo-ayala@hotmail.com>
2020-06-26 14:47:18 -0500

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,12 @@
+class StaticController < ActionController::Base
+   
+    def about
+    end
+
+    def page
+    end
+
+    def some_page
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get('/about', to: 'static#about')
+
+  get('/page', to: 'static#page')
+
+  get('/some_page', to: 'static#some_page')
+
 end
